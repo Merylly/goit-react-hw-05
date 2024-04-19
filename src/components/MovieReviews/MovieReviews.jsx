7a704c8f43094e0b4d9800ab757a67e41e4bd6b2
Merylly <142231027+Merylly@users.noreply.github.com>
2024-04-19 15:43:33 +0300

    fix

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -4,17 +4,19 @@ import css from "./MovieReviews.module.css";
 
 const MovieReviews = () => {
   const { movieReviews } = useMovieDetails();
+  
   return (
     <ul className={css.reviewList}>
-      {Array.isArray(movieReviews) &&
-        movieReviews.map((item) => {
-          return (
-            <li key={item.id}>
-              <h3 className={css.title}>{item.author}</h3>
-              <p className={css.text}>{item.content}</p>
-            </li>
-          );
-        })}
+      {Array.isArray(movieReviews) && movieReviews.length > 0
+        ? movieReviews.map((item) => {
+            return (
+              <li key={item.id}>
+                <h3 className={css.title}>{item.author}</h3>
+                <p className={css.text}>{item.content}</p>
+              </li>
+            );
+          })
+        : "No reviews yet."}
     </ul>
   );
 };
